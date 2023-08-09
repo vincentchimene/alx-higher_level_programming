ec12c649e85c0a48441bf57856333952ce47a85c
Vincent Opurum <vincentchimene@gmail.com>
2023-08-09 17:32:15 +0100

    added task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-# a function that prints a string in uppercase followed by a new line.def uppercase(str):
+# a function that prints a string in uppercase followed by a new line.
+def uppercase(str):
     for i in str:
         i = ord(i)
         if 97 <= i <= 122:
