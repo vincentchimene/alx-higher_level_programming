86308fc84f659bd408abc1ee2cbfb04ec518a6c0
Vincent Opurum <vincentchimene@gmail.com>
2023-08-09 23:54:51 +0100

    replaced 'is' with '=='

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,6 +4,6 @@
 # in reverse order, alternating lowercase and uppercase
 # (z in lowercase and Y in uppercase) , not followed by a new line.
 for a in range(122, 96, -1):
-    if a % 2 is 1:
+    if a % 2 == 1:
         a -= 32
     print("{:c}".format(a), end="")
