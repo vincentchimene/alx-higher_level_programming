df72287a9ea755d6ddcc0570e02779d1610725b1
Vincent Opurum <vincentchimene@gmail.com>
2023-08-08 18:44:42 -0700

    added task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#  A program that prints the ASCII alphabet, in lowercase, not followed by a new line.
-
+#  A program that prints the ASCII alphabet, in lowercase.
 for a in range(97, 123):
     print('{:c}'.format(a), end='')
