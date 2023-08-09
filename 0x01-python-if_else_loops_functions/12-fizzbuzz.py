5423d5e2a60657b8fdb154ee210c6c7fd7d62629
Vincent Opurum <vincentchimene@gmail.com>
2023-08-09 19:04:14 +0100

    adjusted comment lines

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 # a function that prints numbers 1 - 100
-# but replaces multiples of 3 with fizz, 5 with buzz and those of 15 with fizzbuzz
+# but replaces multiples of 3 with fizz,
+# multiples of 5 with buzz and those of 15 with fizzbuzz
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 15) == 0:
