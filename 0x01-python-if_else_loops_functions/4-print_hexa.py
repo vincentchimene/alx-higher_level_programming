cf011d063928e8cd0754787e133a9f59c6c53586
Vincent Opurum <vincentchimene@gmail.com>
2023-08-08 19:08:57 -0700

    remove trailing whitespaces

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-# a program that prints all numbers from 0 to 98 in decimal and in hexadecimal 
+# a program that prints all numbers from 0 to 98 in decimal and in hexadecimal
 for num in range(0, 99):
     print("{:d} = {}".format(num, hex(num)))
