b0926ac54dfbd018fec948499211568818cb6a6c
Vincent Opurum <vincentchimene@gmail.com>
2023-08-09 19:44:20 +0100

    added task 14

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Write a program that prints the ASCII alphabet,
+# in reverse order, alternating lowercase and uppercase
+# (z in lowercase and Y in uppercase) , not followed by a new line.
+for a in range(122, 96, -1):
+    if a % 2 is 1:
+        a -= 32
+    print("{:c}".format(a), end="")
