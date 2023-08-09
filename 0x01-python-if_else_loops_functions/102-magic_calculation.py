7af80e0eaf80c4e6fdebcd7ff7deee661dcefcb9
Vincent Opurum <vincentchimene@gmail.com>
2023-08-10 00:10:29 +0100

    added task 16

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# the Python function def magic_calculation(a, b, c):
+# that does exactly the same as the following Python bytecode
+def magic_calculation(a, b, c):
+    if a < b:
+        return(c)
+    elif c > b:
+        return(a + b)
+    else:
+        return(a * b - c)
