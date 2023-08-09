d69915b1d6ed398ffca420e001c2673062f5ffa4
Vincent Opurum <vincentchimene@gmail.com>
2023-08-09 18:56:17 +0100

    added task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# a function that prints numbers 1 - 100 but replaces multiples of 3 with fizz, 5 with buzz and those of 15 with fizzbuzz
+def fizzbuzz():
+    for i in range(1, 101):
+        if (i % 15) == 0:
+            print("FizzBuzz", end=" ")
+        elif (i % 3) == 0:
+            print("Fizz", end=" ")
+        elif (i % 5) == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
