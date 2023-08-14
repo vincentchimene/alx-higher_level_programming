5ddbc2a9a65aebb6c7fc9e3a1100a9338b4ff5d4
Vincent Opurum <vincentchimene@gmail.com>
2023-08-15 00:21:13 +0100

    removed whitespace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 #  a function that replaces an element of a list at a specific position (like in C).#!/usr/bin/python3
-
 def replace_in_list(my_list, idx, element):
-
     if idx < 0:
         return my_list
     if idx >= len(my_list):
