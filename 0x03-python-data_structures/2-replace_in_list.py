623f34de05e6c7ddfeab82e5a54e6cf2d41968db
Vincent Opurum <vincentchimene@gmail.com>
2023-08-15 00:24:18 +0100

    comment adjuustment

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-#  a function that replaces an element of a list at a specific position (like in C).#!/usr/bin/python3
+#  a function that replaces an element of a list at a specific position (like in C).
+
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
