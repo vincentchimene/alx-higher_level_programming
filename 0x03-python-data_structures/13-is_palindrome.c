beae2a98c89e38651afad4dd62ac1ee7ff6f111b
Vincent Opurum <vincentchimene@gmail.com>
2023-08-15 02:28:55 +0100

    redo of 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,35 +1,35 @@
+#include <stddef.h>
+#include <stdio.h>
 #include "lists.h"
+
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: linked list.
- * Return: 0 if not a palindrome, 1 if a palindrome
+ * is_palindrome - check if a linked list is a palindrome
+ *
+ * @head: first node
+ *
+ * Return: 1 if success
+ *         0 if failed
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *p = NULL;
-	int i = 0, size = 0, j = 0, flag = 1;
-	int *a = NULL;
+	listint_t *tmp = *head;
+	int values[2048], i = 0, cLoop, limit;
 
 	if (head == NULL || *head == NULL)
-		return (flag);
-	p = *head;
-	while (p->next != NULL)
+		return (1);
+
+	while (tmp != NULL)
 	{
-		size++;
-		p = p->next;
-	}
-	a = malloc(sizeof(int) * size + 1);
-	p = *head;
-	while (p != NULL)
-	{
-		a[i] = p->n;
+		values[i] = tmp->n;
 		i++;
-		p = p->next;
+		tmp = tmp->next;
 	}
-	j = size;
-	for (i = 0; i <= size / 2; i++, j--)
-		if (a[i] != a[j])
-			flag = 0;
-	free(a);
-	return (flag);
+
+	limit = (i % 2 == 0) ? i / 2 : (i + 1) / 2;
+
+	for (cLoop = 0; cLoop < limit; cLoop++)
+		if (values[cLoop] != values[i - 1 - cLoop])
+			return (0);
+
+	return (1);
 }
