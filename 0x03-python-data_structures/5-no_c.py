232f5dfdc099e8d6ff67e895203bd645f666199f
Vincent Opurum <vincentchimene@gmail.com>
2023-08-15 01:03:42 +0100

    removed white spaces before comment

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 
 #  a function that removes all characters c and C from a string.
 def no_c(my_string):
-    new_str = ""# a new empty string
+    new_str = ""
     for i in range(len(my_string)):
         if my_string[i] != "c" and my_string[i] != "C":
-            new_str += my_string[i]# add the character to the new empty string
+            new_str += my_string[i]
     return new_str
