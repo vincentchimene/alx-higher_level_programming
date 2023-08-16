e5a781095542395140e9bd177caf79353a0ff051
Vincent Opurum <vincentchimene@gmail.com>
2023-08-16 17:57:33 +0100

    added 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-# a program that imports the function def add(a, b): from the file add_0.py
-# and prints the result of the addition 1 + 2 = 3
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
