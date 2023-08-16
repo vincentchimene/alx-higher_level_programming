44de9823e9771caba0b4eb027e55e2a8e0d0f4c1
Vincent Opurum <vincentchimene@gmail.com>
2023-08-16 19:44:11 +0100

    added task 8

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# prints the alphabet in uppercase, followed by a new line.
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+            return(c)
+    else:
+        return(sub(a, b))
+    return 0
