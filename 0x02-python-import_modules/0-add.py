3659f2735e383020b2f378e6dbf22acc8dfc60bb
Vincent Opurum <vincentchimene@gmail.com>
2023-08-16 17:47:45 +0100

    added task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,6 @@
 # a program that imports the function def add(a, b): from the file add_0.py
 # and prints the result of the addition 1 + 2 = 3
 if __name__ == "__main__":
-
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
