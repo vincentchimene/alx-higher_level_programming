b501ce0ee9cbac130b89be9bfd61f2cd2959bf96
Vincent Opurum <vincentchimene@gmail.com>
2023-08-17 02:41:30 +0100

    adjuusted commjt

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-""" a function that adds all unique
-integers in a list (only once for each integer).
-"""
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for idx, item in enumerate(new_list):
