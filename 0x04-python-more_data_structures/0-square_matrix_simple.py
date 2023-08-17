99ca07f0bdab874465c1536fc1f682b5a476113b
Vincent Opurum <vincentchimene@gmail.com>
2023-08-17 02:16:01 +0100

    corrected the variable name

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sq_matrix = []
-    for row in sq_matrix:
-        for a in row:
-            a = a**2
-    return sq_matrix
+    new_matrix = ([[a**2 for a in row] for row in matrix])
+    return(new_matrix)
