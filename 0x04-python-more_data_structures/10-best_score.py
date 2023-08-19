192776b5136a8fd85b4159ee9068276102a10a01
Vincent Opurum <vincentchimene@gmail.com>
2023-08-19 01:39:09 +0100

    added task10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# returns a key with the biggest integer value.
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    r_value = list(a_dictionary.keys())[0]
+    b_score = list(a_dictionary.values())[0]
+    for k, v in a_dictionary.items():
+        if v > b_score:
+            b_score = v
+            r_value = k
+    return (r_value)
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# returns a new dictionary with all values multiplied by 2.
+def multiply_by_2(a_dictionary):
+    return ({i: a_dictionary[i] * 2 for i in a_dictionary})
