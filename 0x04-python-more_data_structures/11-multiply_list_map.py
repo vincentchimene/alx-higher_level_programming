112ffa8b46eca9da44727fc53d1cd61ca645d67e
Vincent Opurum <vincentchimene@gmail.com>
2023-08-19 02:12:42 +0100

    reduced comment words

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-# returns a list with all values times a number without using any loops.
+# returns a list with all values times a number noloops.
 def mutiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))
