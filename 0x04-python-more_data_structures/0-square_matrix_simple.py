18e99fcf7de11ac9202e7084ba63dc86e5604412
Vincent Opurum <vincentchimene@gmail.com>
2023-08-19 01:32:09 +0100

    added task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-""" Write a function that computes
-the square value of all integers of a matrix.
-Prototype: def square_matrix_simple(matrix=[]):
-"""
+# computes the square value of all integers of a matrix.
 def square_matrix_simple(matrix=[]):
     new_matrix = ([[a**2 for a in row] for row in matrix])
     return (new_matrix)
