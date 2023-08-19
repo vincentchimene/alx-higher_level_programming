74e13aaa5bbcb5aea2a268283f5e306a4b1d5a75
Vincent Opurum <vincentchimene@gmail.com>
2023-08-19 02:21:05 +0100

    added it

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
-# returns a list with all values times a number noloops.
-def mutiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+returns a list with all values times a number no loops.
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    return(list(map(lambda x: x * number, my_list)))
