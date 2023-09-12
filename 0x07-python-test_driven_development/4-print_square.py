a4f539a12c54b870a47f946b3d84d8dd48c22583
Vincent Opurum <vincentchimene@gmail.com>
2023-09-12 12:59:38 +0100

    added the completed tasks

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3if type(size) is not int:
+"""Defines a square-printing function."""
+
+
+def print_square(size):
+    """Print a square with the # character.
+
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
