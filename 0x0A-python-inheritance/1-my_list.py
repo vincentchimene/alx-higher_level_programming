da25c044cb9b5e221901b83aeaff78a15dc95247
Vincent Opurum <vincentchimene@gmail.com>
2023-09-13 19:38:14 +0100

    debugged ttask1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,8 +6,7 @@ The container of MyList class inherit from list
 
 class MyList(list):
     """MyList class that inherits from list"""
-    def __init__(self):
-        super().__init__()
-
+    
     def print_sorted(self):
+        """Print a list in sorted ascending order."""
         print(sorted(self))
