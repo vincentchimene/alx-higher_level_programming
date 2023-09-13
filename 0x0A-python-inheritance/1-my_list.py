9dacb227679bd30dc03e39fe3d2984bf1eeccb1f
Vincent Opurum <vincentchimene@gmail.com>
2023-09-13 19:43:47 +0100

    removed whitespace

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,7 +6,7 @@ The container of MyList class inherit from list
 
 class MyList(list):
     """MyList class that inherits from list"""
-    
+
     def print_sorted(self):
         """Print a list in sorted ascending order."""
         print(sorted(self))
