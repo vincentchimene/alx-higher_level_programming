e360026853c3ad8c1d31e2891c9e5720669ebf72
Vincent Opurum <vincentchimene@gmail.com>
2023-09-20 03:59:35 +0100

    edited the script for pushing

--- 0x0B-python-input_output/push.sh
+++ 0x0B-python-input_output/push.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-# get the argument message
-message="$1"
-
-# If no commit message is passed, use current date time in the commit message
-if [[ -z "${message// }" ]]
-    then
-        message=$(date '+%Y-%m-%d %H:%M:%S')
-fi
-
-# stage all changes
 git add .
-echo "====staged all git files"
-
-# add commit
-git commit -m "$message"
-echo "====added the commit with message: '$message'"
+echo 'Enter the commit message:'
+read commitMessage
+git commit -m "$commitMessage"
 git push
