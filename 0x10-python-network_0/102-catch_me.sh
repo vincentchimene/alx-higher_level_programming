3367f8c6068447d8c4935d2c994339b7588813f1
Vincent Opurum <vincentchimene@gmail.com>
2023-12-01 00:00:46 +0100

    edited the origin value

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!,in the body of the response
-curl -sL -X PUT -H "Origin: ALX School" 0.0.0.0:5000/catch_me_3
+curl -sL -X PUT -H "Origin: ALXSchool" 0.0.0.0:5000/catch_me_3
