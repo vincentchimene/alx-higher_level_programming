2f535cdf38dc0fde4baf26aff0dcd6b2f1bd45aa
Vincent Opurum <vincentchimene@gmail.com>
2023-11-30 23:37:28 +0100

    adds task 6 and its complexity

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A module that returns the peak of the list
+"""
+
+
+def find_peak(list_of_integers):
+    """ A function that returns the peak of the list
+    """
+    if (len(list_of_integers) == 0):
+        return None
+
+    else:
+        peak = list_of_integers[0]
+        for i in range(len(list_of_integers)):
+            if list_of_integers[i] > peak:
+                peak = list_of_integers[i]
+        return peak
