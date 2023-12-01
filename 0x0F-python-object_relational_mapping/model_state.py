aee3fd052791da377efd5ecbf139c8c7036ff7ee
Vincent Opurum <vincentchimene@gmail.com>
2023-12-01 04:09:57 +0100

    adjusted for pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-a python file that contains the class definition of a State 
-and an instance Base = declarative_base():
+a python file that contains the class definition of a State
 """
 
 import sqlalchemy
